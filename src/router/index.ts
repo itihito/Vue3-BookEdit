a82408f0c047a2aed827c52f85681421aaac8abe
itihito <itihito>
2023-12-31 16:43:37 +0900

    未ログイン時に検索画面、編集画面へのアクセスをログイン画面にリダイレクトするように修正

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import BookIndex from "../pages/BookIndex.vue";
 import BookSearch from "../pages/BookSearch.vue";
 import BookEdit from "../pages/BookEdit.vue";
 import Login from "../pages/Login.vue";
+import authStore from "../store/auth/index";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -19,11 +20,17 @@ const routes: Array<RouteRecordRaw> = [
     path: "/search",
     name: "BookSearch",
     component: BookSearch,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/edit/:bookId",
     name: "BookEdit",
     component: BookEdit,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/:catchAll(.*)*",
@@ -34,6 +41,23 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, _from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (!authStore.state.user.uid) {
+      next({
+        path: "/login",
+        query: {
+          redirect: to.fullPath,
+        },
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
