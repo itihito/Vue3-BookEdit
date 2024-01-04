cb51f7ee8888e70f1d52019b4d54a162aacaed38
itihito <itihito>
2024-01-04 14:14:48 +0900

    fix: 感想一覧ボタンの出し分け

--- src/global/header.vue
+++ src/global/header.vue
@@ -11,7 +11,9 @@ const logout = async () => {
     await signOut(auth);
     await router.push({ name: "Login" });
     location.reload();
-  } catch (error) {}
+  } catch (error) {
+    console.error(error);
+  }
 };
 </script>
 
@@ -20,8 +22,8 @@ const logout = async () => {
     <v-app-bar app color="primary" dark>
       <v-btn class="text-h5" href="/">読書感想アプリ</v-btn>
       <v-spacer></v-spacer>
-      <v-btn class="bg-secondary" to="/"> 感想一覧 </v-btn>
       <v-sheet color="primary" v-if="isAuth">
+        <v-btn class="bg-secondary" to="/"> 感想一覧 </v-btn>
         <v-btn class="bg-error ml-4 mr-2" @click="logout"
           >ログアウト<v-icon class="ml-2">mdi-logout</v-icon>
         </v-btn>
