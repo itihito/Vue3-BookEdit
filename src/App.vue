a7d4e2888a71675b5a838443befbcdcc5c9a62f2
itihito <itihito>
2024-01-08 17:41:43 +0900

    fix: delete-local-storageをdelete-booksに名称修正

--- src/App.vue
+++ src/App.vue
@@ -151,7 +151,7 @@ const updateBooks = (newBooks: Book[]) => {
           @add-book-list="addBook"
           @update-book-info="updateBookInfo"
           @delete-Book-List="deleteBook"
-          @delete-local-storage="deleteBooks"
+          @delete-books="deleteBooks"
         />
       </v-container>
     </v-main>
