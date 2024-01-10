c030feabb43957d4a4b340f1d57dce48cff4e80f
itihito <enaoki0818@gmail.com>
2024-01-10 17:30:17 +0900

    feat: ログイン画面、ユーザー作成画面にバリデーションを追加

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase/firebase.ts";
-import { ref, onMounted, computed } from "vue";
+import { ref, onMounted, computed, watch } from "vue";
 import { useStore } from "vuex";
 import { useRouter } from "vue-router";
+import { VForm } from "vuetify/lib/components/index.mjs";
 
 const email = ref<string>("");
 const password = ref<string>("");
@@ -45,44 +46,68 @@ const signIn = () => {
     });
 };
 
-const loginBtnCSS = computed(() => {
-  if (email.value.length >= 1 && password.value.length >= 6) {
-    return "font-weight-bold text-white bg-blue";
-  } else {
-    return "font-weight-bold text-grey-darken-1 bg-grey-lighten-2";
+// v-formのref="form"により自動的にバインドしている変数
+const form = ref<typeof VForm>();
+// バリデーション結果
+let valid = ref<boolean>(false);
+// formの値が変更されるたびにバリデーション結果を更新
+watch([email, password], async () => {
+  // インプット要素が全て入力されている場合
+  if (email.value && password.value) {
+    const validationResult = await form?.value?.validate();
+    valid.value = validationResult?.valid || false;
   }
 });
 
-const loginBtnIsDisabled = computed(() => {
-  if (email.value.length >= 1 && password.value.length >= 6) {
-    return false;
-  } else {
-    return true;
-  }
+const loginBtnCSS = computed(() => {
+  return valid.value
+    ? "font-weight-bold text-white bg-blue"
+    : "font-weight-bold text-grey-darken-1 bg-grey-lighten-2";
 });
+
+const emailRules = [
+  (v: string) => !!v || "メールアドレスは必須です",
+  (v: string) =>
+    /^\S+@\S+\.\S+$/.test(v) || "有効なメールアドレスを入力してください",
+];
+const passwordRules = [
+  (v: string) => !!v || "パスワードは必須です",
+  (v: string) =>
+    (v && v.length >= 6) || "パスワードは6文字以上で入力してください",
+];
 </script>
 
 <template>
   <v-sheet max-width="600px" elevation="10" class="mx-auto">
     <v-container class="bg-teal-lighten-4">
       <v-sheet class="bg-teal-lighten-4 mx-auto" max-width="90%">
-        <v-text-field v-model="email" bg-color="white" label="メールアドレス">
-        </v-text-field>
-        <v-text-field
-          v-model="password"
-          bg-color="white"
-          label="パスワード(6桁以上)"
-          type="password"
-        ></v-text-field>
-        <v-sheet class="bg-teal-lighten-4 mx-auto">
-          <v-btn
-            :class="loginBtnCSS"
-            width="100%"
-            @click="signIn"
-            :disabled="loginBtnIsDisabled"
-            >ログイン</v-btn
+        <v-form ref="form">
+          <v-text-field
+            v-model="email"
+            bg-color="white"
+            label="メールアドレス"
+            :rules="emailRules"
+            required
           >
-        </v-sheet>
+          </v-text-field>
+          <v-text-field
+            v-model="password"
+            bg-color="white"
+            label="パスワード(6桁以上)"
+            type="password"
+            :rules="passwordRules"
+            required
+          ></v-text-field>
+          <v-sheet class="bg-teal-lighten-4 mx-auto">
+            <v-btn
+              :class="loginBtnCSS"
+              width="100%"
+              @click="signIn"
+              :disabled="!valid"
+              >ログイン</v-btn
+            >
+          </v-sheet>
+        </v-form>
       </v-sheet>
     </v-container>
     <v-divider class="border-opacity-50"></v-divider>
--- src/pages/UserRegister.vue
+++ src/pages/UserRegister.vue
@@ -5,9 +5,10 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { auth } from "../firebase/firebase.ts";
-import { ref, onMounted, computed } from "vue";
+import { ref, onMounted, computed, watch } from "vue";
 import { useStore } from "vuex";
 import { useRouter } from "vue-router";
+import { VForm } from "vuetify/lib/components/index.mjs";
 
 const userName = ref<string>("");
 const email = ref<string>("");
@@ -25,9 +26,7 @@ onMounted(() => {
   });
 });
 
-const createAccount = () => {
-  if (email.value === "" || password.value === "") return;
-
+const createUser = async () => {
   createUserWithEmailAndPassword(auth, email.value, password.value)
     .then(async (userCredential) => {
       const user = userCredential.user;
@@ -52,29 +51,39 @@ const createAccount = () => {
     });
 };
 
-const registerBtnCSS = computed(() => {
-  if (
-    userName.value.length >= 1 &&
-    email.value.length >= 1 &&
-    password.value.length >= 6
-  ) {
-    return "font-weight-bold text-white bg-blue";
-  } else {
-    return "font-weight-bold text-grey-darken-1 bg-grey-lighten-2";
+const form = ref<typeof VForm>();
+let valid = ref<boolean>(false);
+// formの値が変更されるたびにバリデーション結果をを更新
+watch([userName, email, password], async () => {
+  // インプット要素が全て入力されている場合
+  if (userName.value && email.value && password.value) {
+    const validationResult = await form?.value?.validate();
+    valid.value = validationResult?.valid || false;
   }
 });
 
-const registerBtnIsDisabled = computed(() => {
-  if (
-    userName.value.length >= 1 &&
-    email.value.length >= 1 &&
-    password.value.length >= 6
-  ) {
-    return false;
-  } else {
-    return true;
-  }
+const registerBtnCSS = computed(() => {
+  return valid.value
+    ? "font-weight-bold text-white bg-blue"
+    : "font-weight-bold text-grey-darken-1 bg-grey-lighten-2";
 });
+
+const nameRules = [
+  (v: string) => !!v || "ユーザー名は必須です",
+  (v: string) =>
+    (v && v.length >= 4) || "ユーザー名は4文字以上入力してください",
+  (v: string) => (v && v.length <= 10) || "ユーザー名は10文字まで入力可能です",
+];
+const emailRules = [
+  (v: string) => !!v || "メールアドレスは必須です",
+  (v: string) =>
+    /^\S+@\S+\.\S+$/.test(v) || "有効なメールアドレスを入力してください",
+];
+const passwordRules = [
+  (v: string) => !!v || "パスワードは必須です",
+  (v: string) =>
+    (v && v.length >= 6) || "パスワードは6文字以上で入力してください",
+];
 </script>
 
 <template>
@@ -82,25 +91,41 @@ const registerBtnIsDisabled = computed(() => {
     <v-sheet max-width="600px" elevation="10" class="mx-auto">
       <v-container class="bg-teal-lighten-4">
         <v-sheet class="bg-teal-lighten-4 mx-auto" max-width="90%">
-          <v-text-field v-model="userName" bg-color="white" label="ユーザー名">
-          </v-text-field>
-          <v-text-field v-model="email" bg-color="white" label="メールアドレス">
-          </v-text-field>
-          <v-text-field
-            v-model="password"
-            bg-color="white"
-            label="パスワード(6桁以上)"
-            type="password"
-          ></v-text-field>
-          <v-sheet class="bg-teal-lighten-4 d-flex justify-center">
-            <v-btn
-              :class="registerBtnCSS"
-              width="100%"
-              @click="createAccount"
-              :disabled="registerBtnIsDisabled"
-              >ユーザー登録</v-btn
+          <v-form ref="form">
+            <v-text-field
+              v-model="userName"
+              bg-color="white"
+              label="ユーザー名"
+              :rules="nameRules"
+              required
             >
-          </v-sheet>
+            </v-text-field>
+            <v-text-field
+              v-model="email"
+              bg-color="white"
+              label="メールアドレス"
+              :rules="emailRules"
+              required
+            >
+            </v-text-field>
+            <v-text-field
+              v-model="password"
+              bg-color="white"
+              label="パスワード(6桁以上)"
+              type="password"
+              :rules="passwordRules"
+              required
+            ></v-text-field>
+            <v-sheet class="bg-teal-lighten-4 d-flex justify-center">
+              <v-btn
+                :class="registerBtnCSS"
+                width="100%"
+                @click="createUser"
+                :disabled="!valid"
+                >ユーザー登録</v-btn
+              >
+            </v-sheet>
+          </v-form>
         </v-sheet>
       </v-container>
       <v-divider class="border-opacity-50"></v-divider>
